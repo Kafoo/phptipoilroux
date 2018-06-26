10861d61e82cb824c4da5e24b2e0d50b722a999b
Hydeo <p.garciapelayo@gmail.com>
2018-06-26 22:29:11 +0200

    Ajout Sommaire

---
+++ SOMMAIRE/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>SOMMAIRE</title>
+</head>
+<body>
+	<h1>
+		SOMMAIRE
+	</h1>
+	<p>
+		<a href="..\BLAGUES\index.php">Blague</a>
+	</p>
+	<p>
+		<a href="..\TAPCAZ\index.php">Tapcaz</a>
+	</p>
+</body>
+</html>
