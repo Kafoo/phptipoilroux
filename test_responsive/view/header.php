f74d152c51a7fddea0c98a79de50da440781624e
Unknown <ant.guillard@gmail.com>
2018-10-14 01:19:26 +0200

    TR-SS update

--- test_responsive/view/header.php
+++ test_responsive/view/header.php
@@ -1,25 +1,24 @@
 <!-- -------- HEADER -------- -->
 <header>
 
-		<div id="branding">
-			<img src="img/global/header_title.png" id="headerTitle" style="cursor: pointer;" onclick="window.location='accueil.php';">
+		
+		<img src="img/global/header_title.png" id="branding" style="cursor: pointer;" onclick="window.location='accueil.php';">
+		
+		<div id="connection">
+			
 		</div>
 
-		<div id="connection">
-			-------- CONNECTION --------
+	<!----- NAVIGATION ----->
+	<nav>
+		<div class="centering">
+			<ul>
+				<li><a href="home.php">ACCUEIL</a></li>|
+				<li>AVENTURES</li>|
+				<li>SOCIAL</li>|
+				<li><a href="profil.php">PROFIL</a></li>|
+				<li>HELP</li>
+			</ul>
 		</div>
+	</nav>
 
 </header>
-
-<!-- -------- NAVIGATION -------- -->
-<nav>
-	<div class="centering">
-		<ul>
-			<li><a href="home.php">ACCUEIL</a></li>|
-			<li>AVENTURES</li>|
-			<li>SOCIAL</li>|
-			<li><a href="profil.php">PROFIL</a></li>|
-			<li>HELP</li>
-		</ul>
-	</div>
-</nav>
