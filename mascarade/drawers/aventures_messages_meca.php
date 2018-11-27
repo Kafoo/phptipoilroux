34644697a5c570cf195be6eb645d434766f446e8
Unknown <ant.guillard@gmail.com>
2018-11-27 16:06:35 +0100

    MAS advanced diceRoll

--- mascarade/drawers/aventures_messages_meca.php
+++ mascarade/drawers/aventures_messages_meca.php
@@ -41,21 +41,25 @@ if ($info['type'] === 'diceRoll_GM') {
 			
 			<div class="diceRollBox">
 				<div class="diceRollPerso">
-					<b><u><?=$perso?></u></b><br>
-					<i>(<?=ucfirst($carac)?>)</i><br>
+					<div class="box">						
+						<b><u><?=$perso?></u></b>
+					</div>
+					<div class="box">
+						<i>(<?=ucfirst($carac)?>)</i>
+					</div>
 
 					<?php
 					if ($diceRoll['persoID'] == $persoID) {
 					// IF CURRENT USER
 					if ($result == 0) { ?>
 						<!-- Not Rolled -->
-						<div class="rollBox rollTheDie button"
+						<div class="box rollBox rollTheDie button"
 						ajax="?action=rollTheDie&rollID=<?=$diceRoll[0]?>"
 						>Lance le dé !</div>
 					<?php
 					}else{ ?>
 						<!-- Rolled -->
-						<div class="rollBox dieRolled">
+						<div class="box rollBox dieRolled">
 							Résultat du jet : <?=$result?>
 						</div>
 					<?php
@@ -64,11 +68,11 @@ if ($info['type'] === 'diceRoll_GM') {
 					// IF OTHER USER
 					if ($result == 0) { ?>
 						<!-- Not Rolled -->
-						<div class="rollBox">En attente</div>
+						<div class="box rollBox">En attente</div>
 					<?php
 					}else{ ?>
 						<!-- Rolled -->
-						<div class="rollBox dieRolled">
+						<div class="box rollBox dieRolled">
 							Résultat : <?=$result?>			
 						</div>
 					<?php
@@ -79,65 +83,31 @@ if ($info['type'] === 'diceRoll_GM') {
 				</div>
 				<div class="centering">
 					<div class="diceRollDigits">
-						<div class="diceRollDigit digit-roll">9</div>
-						<div class="diceRollDigit digit-carac">+2</div>
-						<div class="diceRollDigit digit-bonus">+3</div>
-						<div class="diceRollDigit digit-malus">-1</div>
-						<span style="font-weight: bolder">=</span>	
-						<div class="diceRollDigit digit-resultFinal">
-							13
-							<span class="digit-difficulty"> /12</span>
-							<img src="./img/icones/tic_no.png" class="diceRoll-tic">
+						<div class="diceRollDigit digit-roll"><?=$result?></div>
+						<div class="diceRollDigit digit-carac">+<?=$valCarac?></div>
+						<div class="diceRollDigit digit-bonus">+<?=$bonus?></div>
+						<div class="diceRollDigit digit-malus">-<?=$malus?></div>
+						<div class="inline">
+							<span style="font-weight: bolder">=</span>	
+							<div class="diceRollDigit digit-resultFinal">
+								<?=$resultFinal?>
+								<span class="digit-difficulty"> /<?=$difficulty?></span>
+								<?php
+								if ($resultFinal>=$difficulty) {
+									echo '<img src="./img/icones/tic_yes.png" class="diceRoll-tic">';
+								}else{
+									echo '<img src="./img/icones/tic_no.png" class="diceRoll-tic">';
+								}?>
+							</div>
+						</div>
+						<div class="resultText">
+							<?php
+							if ($resultFinal>=$difficulty) {echo '<div class="yes">Réussi !</div>';}
+							else{echo '<div class="no">Raté !</div>';}
+							?>
 						</div>
 					</div>
 				</div>
-
-
-
-				<?php /*
-				if ($diceRoll['persoID'] == $persoID) {
-					// IF CURRENT USER
-					if ($result == 0) { ?>
-						<!-- Not Rolled -->
-						<div class="rollBox rollTheDie button"
-						ajax="?action=rollTheDie&rollID=<?=$diceRoll[0]?>"
-						>Lance le dé !</div>
-					<?php
-					}else{ ?>
-						<!-- Rolled -->
-						<div class="rollBox dieRolled">
-							Résultat du jet : <?=$result?>
-						</div>
-					<?php
-					}
-				}else{
-					// IF OTHER USER
-					if ($result == 0) { ?>
-						<!-- Not Rolled -->
-						<div class="rollBox">En attente</div>
-					<?php
-					}else{ ?>
-						<!-- Rolled -->
-						<div class="rollBox dieRolled">
-							Résultat du jet : <?=$result?>			
-						</div>
-					<?php
-					}
-				}//endif other user ?> 
-
-				<div class="rollBox">
-					<?php
-					echo $result." + ".$valCarac." + ".$bonus." - ".$malus." = ".$resultFinal."/".$difficulty." - ";
-					if ($resultFinal >= $difficulty) {
-						echo "Lancé Réussi !";
-					}else{
-						echo "Lancé raté !";
-					}
-					?>
-				</div> <?php */ ?>
-
-
-
 			</div>
 		<?php
 		} ?>
@@ -159,11 +129,11 @@ if ($info['type'] === 'diceRoll_player') {
 
 	<div >
 		<img src="img/icones/jet_white"
-		class="diceRoll_icone">
+		class="diceRoll_icone desktop">
 	</div>
-	<div class="diceRollBloc centering">
+	<div class="diceRollBloc">
 
-		<div class="diceRollTitle">
+		<div class="diceRollTitle centering">
 			Lancé de dés !<br>
 			<span class="diceRollContent">- <?=$info['contenu']?> -</span>
 		</div>
@@ -174,59 +144,92 @@ if ($info['type'] === 'diceRoll_player') {
 			$diceRoll = $diceRolls[$j];
 			$perso = $diceRoll['nom'];
 			$carac = $diceRoll['carac'];
-			$goal = $diceRoll[$carac];
+			$valCarac = $diceRoll[$carac];
 			$result = $diceRoll['result'];
 			$bonus = $diceRoll['bonus'];
 			$malus = $diceRoll['malus'];
-			$resultFinal = floatval($result)-floatval($bonus)+floatval($malus);
+			$difficulty = $diceRoll['difficulty'];
+			$resultFinal = floatval($result)+floatval($valCarac)+floatval($bonus)-floatval($malus);
 
-			if ($goal<$resultFinal) {$success = 1;}
-			elseif ($goal>=$resultFinal) {$success = 0;}
+			if ($difficulty<$resultFinal) {$success = 1;}
+			elseif ($difficulty>=$resultFinal) {$success = 0;}
 
 			?>
 			
 			<div class="diceRollBox">
-				<u><?=$perso?></u> <i>(<?=ucfirst($carac)?>)</i>
+				<div class="diceRollPerso">
+					<div class="box">						
+						<b><u><?=$perso?></u></b>
+					</div>
+					<div class="box">
+						<i>(<?=ucfirst($carac)?>)</i>
+					</div>
 
-				<?php
-				if ($diceRoll['persoID'] == $persoID) {
+					<?php
+					if ($diceRoll['persoID'] == $persoID) {
+					// IF CURRENT USER
 					if ($result == 0) { ?>
-						<div class="rollBox rollTheDie button"
+						<!-- Not Rolled -->
+						<div class="box rollBox rollTheDie button"
 						ajax="?action=rollTheDie&rollID=<?=$diceRoll[0]?>"
 						>Lance le dé !</div>
 					<?php
 					}else{ ?>
-						<div class="rollBox dieRolled">
-							<?php 
-							echo $result;
-							if ($bonus > 0) {echo " - ".$bonus;}
-							if ($malus > 0) {echo " + ".$malus;}
-							if ($bonus > 0 OR $malus > 0) {echo " = ".$result;}
-							?>													
+						<!-- Rolled -->
+						<div class="box rollBox dieRolled">
+							Résultat du jet : <?=$result?>
 						</div>
 					<?php
 					}
 				}else{
+					// IF OTHER USER
 					if ($result == 0) { ?>
-						<div class="rollBox">En attente</div>
+						<!-- Not Rolled -->
+						<div class="box rollBox">En attente</div>
 					<?php
 					}else{ ?>
-						<div class="rollBox dieRolled">
-							<?php
-							echo $result;
-							if ($bonus > 0) {echo " - ".$bonus;}
-							if ($malus > 0) {echo " + ".$malus;}
-							if ($bonus > 0 OR $malus > 0) {echo " = ".$resultFinal;}
-							?>			
+						<!-- Rolled -->
+						<div class="box rollBox dieRolled">
+							Résultat : <?=$result?>			
 						</div>
 					<?php
-					}
-				}?>
+					} ?>
+				<?php
+				} ?>
+
+				</div>
+				<div class="centering">
+					<div class="diceRollDigits">
+						<div class="diceRollDigit digit-roll"><?=$result?></div>
+						<div class="diceRollDigit digit-carac">+<?=$valCarac?></div>
+						<div class="diceRollDigit digit-bonus">+<?=$bonus?></div>
+						<div class="diceRollDigit digit-malus">-<?=$malus?></div>
+						<div class="inline">
+							<span style="font-weight: bolder">=</span>	
+							<div class="diceRollDigit digit-resultFinal">
+								<?=$resultFinal?>
+								<span class="digit-difficulty"> /<?=$difficulty?></span>
+								<?php
+								if ($resultFinal>=$difficulty) {
+									echo '<img src="./img/icones/tic_yes.png" class="diceRoll-tic">';
+								}else{
+									echo '<img src="./img/icones/tic_no.png" class="diceRoll-tic">';
+								}?>
+							</div>
+						</div>
+						<div class="resultText">
+							<?php
+							if ($resultFinal>=$difficulty) {echo '<div class="yes">Réussi !</div>';}
+							else{echo '<div class="no">Raté !</div>';}
+							?>
+						</div>
+					</div>
+				</div>
 			</div>
 		<?php
 		} ?>
 	</div>
 	<!------ FIXINFO ------>
-	<?php include('drawers/aventures_fixinfos.php');?>
-<?php
-} ?>
+	<?php include('drawers/aventures_fixinfos.php');
+
+} //endif diceroll from user ?>
