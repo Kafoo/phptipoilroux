e648f9c3ac1faa2cc53a7391b9fc32bf971274e7
Unknown <ant.guillard@gmail.com>
2019-01-19 22:01:45 +0100

    test2

--- mascarade/drawers/aventures_fixinfos.php
+++ mascarade/drawers/aventures_fixinfos.php
@@ -2,26 +2,47 @@
 if ($i == 0) {//Seulement pour le 1er post ?>
 	<div class="fixInfos desktop">
 		<?php
-		for ($j=0; $j < count($coterie); $j++) { 
-			if ($coterie[$j]['nom']!=='GM') { //On n'affiche pas le GM ?>
-				<div class="infoPerso">
-					<div class="infoPersoNom"><?=$coterie[$j]['nom']?></div>
- 					<div class="infoPersoPU" hidden>
-						<b>LVL : </b><?=$coterie[$j]['lvl']?><br><br>
-						<b>Clan : </b><?=ucfirst($coterie[$j]['clan'])?><br>
+		for ($j=0; $j < count($coterie); $j++) {  
+			if ($coterie[$j]['nom']!=='GM') { //On n'affiche pas le GM ?> 
+				<div class="infoPerso"> 
+					<?php // Etoiles autour du nom si perso du user 
+					if ($coterie[$j]['userID'] === $_SESSION['id']) { ?> 
+						<img src="img/icones/monperso.png" style="width: 10px;"> 
+						<div class="infoPersoNom"><?=$coterie[$j]['nom']?></div> 
+						<img src="img/icones/monperso.png" style="width: 10px;"> 
+					<?php 
+					} else {?> 
+						<div class="infoPersoNom"><a href="profil.php?persoID=<?=$coterie[$j]['persoID']?>"><?=$coterie[$j]['nom']?></a></div> 
+ 
+					<?php 
+					} ?> 
+ 
+ 					<div class="infoPersoPU" hidden> 
+						<b>LVL : </b><?=$coterie[$j]['lvl']?><br><br> 
+						<b>Clan : </b><?=ucfirst($coterie[$j]['clan'])?><br> 
 						<b>Nature : </b><?=$coterie[$j]['nature']?><br>
 						<b>Attitude : </b><?=$coterie[$j]['attitude']?><br>
 						<b>Concept : </b><?=$coterie[$j]['concept']?><br>
-						<b>Défaut : </b><?=$coterie[$j]['defaut']?><br><br>
-						<b>Discipline : </b><?=ucfirst($coterie[$j]['nom_discipline'])?><br>
-					</div>
-					<div class="infoPersoDropdown">
-						<img src="img/rpg/pv_<?=$coterie[$j]['pv']?>.png" class="hpBar">
-						<div class="infoPersoLvl">lvl <?=$coterie[$j]['lvl']?></div>
-						<div class="infoPersoXP"></div>
-						<div class="infoPersoCarac logo-carac1"><?=$coterie[$j]['forc']?></div>
-						<div class="infoPersoCarac logo-carac2"><?=$coterie[$j]['dexterite']?></div>
-						<div class="infoPersoCarac logo-carac3"><?=$coterie[$j]['intelligence']?></div>
+						<b>Défaut : </b><?=$coterie[$j]['defaut']?><br><br> 
+						<b>Discipline : </b><?=ucfirst($coterie[$j]['nom_discipline'])?><br> 
+					</div> 
+ 
+					<?php //Calcul du pourcentage vers le nextLVL 
+						$xp = $coterie[$j]['xp']; 
+						$nextLVL = $coterie[$j]['nextLVL']; 
+						$pourcent = $xp*100/$nextLVL; 
+					?> 
+ 
+					<div class="infoPersoDropdown"> 
+						<img src="img/rpg/pv_<?=$coterie[$j]['pv']?>.png" class="hpBar"> 
+						<div class="infoPersoLvl">lvl <?=$coterie[$j]['lvl']?></div> 
+						<div class="infoPersoXP"  
+						style="background: linear-gradient(to right, #5154bd <?=$pourcent?>%, rgb(200,200,200) <?=$pourcent?>%);"> 
+							<?=$xp?> / <?=$nextLVL?> XP 
+						</div> 
+						<div class="infoPersoCarac logo-carac1"><?=$coterie[$j]['forc']?></div> 
+						<div class="infoPersoCarac logo-carac2"><?=$coterie[$j]['dexterite']?></div> 
+						<div class="infoPersoCarac logo-carac3"><?=$coterie[$j]['intelligence']?></div> 
 						<div class="infoPersoCarac logo-carac4"><?=$coterie[$j]['charisme']?></div>
 						<div class="infoPersoCarac logo-carac5"><?=$coterie[$j]['perception']?></div>
 					</div>
