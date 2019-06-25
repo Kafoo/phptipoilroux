659d4640d17e8ece632b979f8f4953406e4c579b
Unknown <ant.guillard@gmail.com>
2019-06-25 13:37:10 +0200

    MAS minupdate
    debug du phpmailer

--- mascarade/_shared_/functions.php
+++ mascarade/_shared_/functions.php
@@ -144,7 +144,7 @@ function send_mail ($addresses, $subject, $body, $altBody){
 
     try {
         //Server settings
-        $mail->SMTPDebug = 0;                                 // Enable verbose debug output
+        $mail->SMTPDebug = 2;                                 // Enable verbose debug output
         $mail->isSMTP();                                      // Set mailer to use SMTP
         $mail->Host = 'smtp.gmail.com';  // Specify main and backup SMTP servers
         $mail->SMTPAuth = true;                               // Enable SMTP authentication
