7f1516bebb1b5f4fcabdc4707424625cb13c3787
Unknown <ant.guillard@gmail.com>
2019-07-21 23:17:50 +0200

    MAS minupdate

--- mascarade/_shared_/connectDB.php
+++ mascarade/_shared_/connectDB.php
@@ -1,9 +1,13 @@
 <?php
 
 $onlinebdd = 1;
+$db_host = getenv('DB_HOST');
+$db_name = getenv('DB_NAME');
+$db_user = getenv('DB_USER');
+$db_password = getenv('DB_PASSWORD');
 
 if ($onlinebdd == 0) {
 	$bdd = new PDO('mysql:host=127.0.0.1;dbname=heroku_3ca6f2b572bf369', "root", "");
 }else{
-	$bdd = new PDO('mysql:host=eu-cdbr-west-02.cleardb.net;dbname=heroku_3ca6f2b572bf369', "b2e27d049f1274", "0ffbbb07");
+	$bdd = new PDO('mysql:host='.$db_host.';dbname='.$db_name, $db_user, $db_password);
 };
